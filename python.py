4ca6914b97fad8832d237a58fb3aef16143c00ab
Vansh <vanshdoc7@gmail.com>
2025-12-04 10:25:58 +0530

    001

--- python.py
+++ python.py
@@ -1,5 +1,22 @@
-num1 = int( input("Enter a value :"))
-num2 = int( input("Enter a value :"))
+# num1 = int( input("Enter a value :"))
+# num2 = int( input("Enter a value :"))
 
-sum = num1 + num2
-print(sum)
+# sum = num1 + num2/2
+# print(sum)
+
+
+# string ===========================================
+
+# str1 = "This is a string."
+# str2 = """Apna College"""
+# str3 = 'Hi Vanssh'
+
+# print(str1, str2, str3)
+
+# str1 = "This is a string.\ntdbjkwnbdkjwqndkldmqwld"  \n   \t
+# print(str1)
+
+str1 = "vansh\t"
+str2 = "dhiman"
+final = str1+str2
+print(final)
