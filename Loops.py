19878c7f7ca88efcbbbffff363acdd063caac96f
Vansh <vanshdoc7@gmail.com>
2025-12-08 11:34:47 +0530

    002

--- Loops.py
+++ Loops.py
@@ -9,4 +9,18 @@ i = 10
 while i >= 1 :
     print(i)
     i -= 1
-# ========================================
+# ======================================== Questions 
+# print number 1 - 100.
+
+num = 1
+while num <= 100:
+    print(num)
+    num += 1
+
+
+
+i = 3
+v = 1
+while v <= 10:
+    print(i, "X", v, "=", i * v)
+    v += 1
