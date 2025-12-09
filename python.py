8d20a8a93d7d1f39d2592caa82b5596a158b0f8f
Vansh <vanshdoc7@gmail.com>
2025-12-09 14:19:19 +0530

    002

--- python.py
+++ python.py
@@ -196,53 +196,60 @@
 #     pass                                                              #pass
 
 # # print("hihhiihiih") 
-# n = 78
-# sum = 0
-# for i in range( n+1):
-#     sum += i
+# # n = 78
+# # sum = 0
+# # for i in range( n+1):
+# #     sum += i
 
-#     print(sum)
-# i=1
-# while i <= n:
-#     sum += i
-#     i += 1 
-# print(i)    
+# #     print(sum)
+# # i=1
+# # while i <= n:
+# #     sum += i
+# #     i += 1 
+# # print(i)    
 
-# countValue = "Vansh dhiman"
-# a = countValue.count("a")
-# print(a)
+# # countValue = "Vansh dhiman"
+# # a = countValue.count("a")
+# # print(a)
 
-# countVelue = "vansh dhiman"
-# target = 'a'
-# countOf = 0
-# for el in countVelue:
-#     if el == target:
-#         countOf += 1
-# print(countOf)        
+# # countVelue = "vansh dhiman"
+# # target = 'a'
+# # countOf = 0
+# # for el in countVelue:
+# #     if el == target:
+# #         countOf += 1
+# # print(countOf)        
     
-# def sum(a, b):
-#     b = a+b
-#     print(b)
+# # def sum(a, b):
+# #     b = a+b
+# #     print(b)
 
-# a =5
-# b = 4
-# sum(a, b)
-print(57+71+64+68+46+58+48)
-print(64+55+87+75+29+25+30+25)
-a = 390
-b = 412
-# b = 55
-# c = 87
-# d = 75
-# e = 29+25
-# f = 30+25
-def calcu(b):
-    sum = b
-    avg = sum/6
-    print(avg)
-    return avg
+# # a =5
+# # b = 4
+# # sum(a, b)
+# print(57+71+64+68+46+58+48)
+# print(64+55+87+75+29+25+30+25)
+# a = 390
+# b = 412
+# # b = 55
+# # c = 87
+# # d = 75
+# # e = 29+25
+# # f = 30+25
+# def calcu(b):
+#     sum = b
+#     avg = sum/6
+#     print(avg)
+#     return avg
 
-calcu(b) 
+# calcu(b) 
 
 
-print(412/600*100)
+# print(412/600*100)
+
+
+# def cal(a, b):
+#     print(a*b)
+#     return a*b
+
+# # cal(2,)
