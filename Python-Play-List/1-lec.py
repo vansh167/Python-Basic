69c6c59e08dd6063bc1082d85d269f088aeca8e5
Vansh <vanshdoc7@gmail.com>
2025-12-28 13:24:46 +0530

    loops

--- Python-Play-List/1-lec.py
+++ Python-Play-List/1-lec.py
@@ -170,9 +170,13 @@
 
 
 
-for i in range(3):                                     #rows
-     for j in range(3):                                  #column
-         print("*", end=" ")
-     print()        
+# for i in range(3):                                     #rows
+#      for j in range(3):                                  #column
+#          print("*", end=" ")
+#      print()        
 
 
+for i in range(1,4):
+     for j in range(i):
+        print("*", end=" ")
+     print()   
