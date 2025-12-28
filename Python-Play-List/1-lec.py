1b196af0982d51b48b7e58b3eead50792454107b
Vansh <vanshdoc7@gmail.com>
2025-12-28 14:43:21 +0530

    while loop

--- Python-Play-List/1-lec.py
+++ Python-Play-List/1-lec.py
@@ -254,7 +254,14 @@
 #         continue
 #     print(i) 
 
-a = 1
-while a <= 10:
-    print(a)
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+a = 0
+while a < 10:
     a += 1
+    if a == 7:
+        break
+    print(a)
