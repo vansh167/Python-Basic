e7b86a65463f558eddd441fcd1b9017401b92a38
Vansh <vanshdoc7@gmail.com>
2025-12-29 06:52:26 +0530

    functions

--- Examples/functions.py
+++ Examples/functions.py
@@ -16,13 +16,31 @@
 # result = add(5,5)
 
 # print(result)
-def classify(n):
-    if n > 0 :
-        return "Positive"
-    elif n < 0 :
-        return "Negative"
-    else :
-        return "zero"
+# def classify(n):
+#     if n > 0 :
+#         return "Positive"
+#     elif n < 0 :
+#         return "Negative"
+#     else :
+#         return "zero"
     
     
-print(classify(22))
+# print(classify(22))
+# def countp(data):
+#     count = 0
+#     neg = 0
+#     for d in data:
+#      if d > 0:
+#         count += 1
+#     return count    
+    
+# nums = [3, -1, 5, 0, 7]
+# print(countp(nums))
+
+def guess(data):
+        if data > 0:
+            return "EVEN"
+        elif data < 0:
+              return "odd"
+print(guess(2)) 
+print(guess(-3))        
