a4f5425146a464b031f065d077c7ad33558b69f4
ajay <ajay_seasec@outlook.com>
2026-01-01 13:29:11 +0530

    loops questions

--- Python-Play-List/Questions.py
+++ Python-Play-List/Questions.py
@@ -17,10 +17,43 @@
 #     print()    
 
 
-for i in range(6):
-    j = 1
-    for j in range(1,i+1):
-        print(j, end=' ')
-        j = j+1
-    i = i+1
-    print()    
+# for i in range(6):
+#     j = 1
+#     for j in range(1,i+1):
+#         print(j, end=' ')
+#         j = j+1
+#     i = i+1
+#     print()    
+
+# Exercise 3: Calculate sum of all numbers from 1 to a given number
+# user = int(input("Enter your number here:"))
+# sum = 0
+# for i in range(1,user+1):
+#       sum += i 
+
+# print(sum)
+
+
+# Exercise 4: Print multiplication table of a given number
+
+
+# user = int(input("Enter your number here:"))
+
+# for i in range(user, 11):
+
+#       sum = user*i
+
+# print(i,sum)
+
+n = 5
+# upper half
+for i in range(n):
+    print(" " * (n - i - 1) + "*" * (2*i + 1))
+
+# lower half
+for i in range(n-2, -1, -1):
+    print(" " * (n - i - 1) + "*" * (2*i + 1))
+
+
+for i in range(5):
+    print(i)
