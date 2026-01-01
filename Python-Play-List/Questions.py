f36091de445b33c4e839c8ff2482d332580b9d63
ajay <ajay_seasec@outlook.com>
2026-01-01 11:58:31 +0530

    Q2

--- Python-Play-List/Questions.py
+++ Python-Play-List/Questions.py
@@ -1,6 +1,26 @@
 # Exercise 1: Print first 10 natural numbers using while loop
 
-num = 0
-while num < 10:
-    num += 1
-    print(num)  
+# num = 0
+# while num < 10:
+#     num += 1
+#     print(num)  
+
+# Exercise 2: Print the following pattern  : 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i, end="")
+#     print()    
+
+
+for i in range(6):
+    j = 1
+    for j in range(1,i+1):
+        print(j, end=' ')
+        j = j+1
+    i = i+1
+    print()    
