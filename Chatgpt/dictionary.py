6a1ae46ed068cb41fa473c0bfaf22481859d3568
Vansh <vanshdoc7@gmail.com>
2026-01-07 23:00:51 +0530

    005

--- Chatgpt/dictionary.py
+++ Chatgpt/dictionary.py
@@ -15,3 +15,37 @@
 
 # data=[1,2,3,4,54]    
 # print(count(data))
+
+
+
+# def two():
+#     return 5,9
+# a,b = two()
+# print(a,b)
+
+
+
+# def total():
+#     return  {"Total:":1}
+
+# print(total())    
+
+
+def dic(data):
+    even = 0
+    odd = 0
+
+    for n in data:
+        if n == 0:
+            continue
+        if n % 2 == 0 :
+            even += 1
+        else:
+            odd += 1
+    return {
+        "even": even,
+        "odd": odd
+    }        
+
+print(dic([1,2,3,445,5,5,56,6,6,6,3]))
+
