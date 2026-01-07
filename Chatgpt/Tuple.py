b5b3886577104a80e02d7039519186f2312cd244
Vansh <vanshdoc7@gmail.com>
2026-01-07 22:18:36 +0530

    002 Dictionary

--- Chatgpt/Tuple.py
+++ Chatgpt/Tuple.py
@@ -1,4 +1,4 @@
-def demo():
-    return 10,2,6
-a,b = demo()
-print(a,b)
+# def demo():
+#     return 10,2,6
+# a,b = demo()
+# print(a,b)
---
+++ Chatgpt/dictionary.py
@@ -0,0 +1,3 @@
+def count(data):
+    return {"total":len(data)}
+print(count([1,3,2,4,4]))
