921166abff2f3c06fc87389b6d6ed5b37eadd43e
Vansh <vanshdoc7@gmail.com>
2026-01-09 23:06:42 +0530

    002 new-D

--- Chatgpt/1-lec.py
+++ Chatgpt/1-lec.py
@@ -391,8 +391,14 @@
 #     }                
 # print(check([1,2,3,4,5,6,-6,-5,-4,-1,0,0]))
 
+# n = 5
+# for i in range(1, n+1):
+#     for  j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
+
 n = 5
-for i in range(1,n+1):
-    for  j in range(1, i+1):
-        print(j, end=" ")
-    print()
+for i in range(1, n+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()    
