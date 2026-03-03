d2c8a539e69f6d9694c53d445342ccbe5dcf50ed
Vansh167 <vanshdoc7@gmail.com>
2026-03-04 00:19:50 +0530

    push code day 1

---
+++ Chatgpt/Start-Again-DAY-1/Basics.py
@@ -0,0 +1,36 @@
+# age = int(input("Enter youe age:"))
+
+
+# if age >= 18:
+#     print("You can Vote")
+# else :
+#     print("You can't Vote")    
+
+# cal = int(input("Enter number A:"))
+# cal2 = int(input("Enter number B:"))
+
+# if cal % cal2 == 0:
+#     print("Yes tris is true")
+# else:
+#     print("Wronf kartik")
+
+
+
+# check = int(input("Enter the Number: "))
+
+# if check % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")    
+
+
+
+
+
+# ----------------------------------------------- Loops  --------------------------------------------------------
+
+for i in range(10):
+    print(i)
+
+for bv in range(2,6):
+    print(bv)
